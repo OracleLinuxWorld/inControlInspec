3abeb711f4345540f7d91663cccca63bf4c76c44
Emiel Kuiper <emielkuiper@gmail.com>
2018-06-19 18:49:00 +0200

    Rewrote checks and jq parser

--- TB_Linux/controls/TB_Linux_controlID_0.rb
+++ TB_Linux/controls/TB_Linux_controlID_0.rb
@@ -1,21 +1,27 @@
-title 'Handling of removable media'
+# Columns in final CSV / Excel sheet:
+# controlID	-> "Control ID"
+# title		-> "Name"
+# checkID	-> "Check ID"
+# desc		-> "Description"
+# describe	-> "Result"
 
-control 'TB_Linux_controlID_0.1' do		# A unique ID for this control
+
+control 'TB_Linux_controlID_0.1' do		
   impact 0.4                                	
-  title 'Verify that /etc/modprobe.d and underlying files are only writeable by the root user'
-  desc 'Various checks regarding modprobe.d modules for loadable modules for eg floppy, USB disk etc'
-  tag controlID: '0'
-  tag checkID: '1'
+  title 'Handling of removable media (physical and virtual)'	
+  desc 'Verify that /etc/modprobe.d is a directory'		
+  tag controlID: '0'						
+  tag checkID: '1'						
   describe file('/etc/modprobe.d') do       
-    it { should be_directory }
+    it { should be_directory }				 	
   end
 end
 
 
-control 'TB_Linux_controlID_0.2' do		# A unique ID for this control
+control 'TB_Linux_controlID_0.2' do		
   impact 0.4                                	
-  title 'Verify that /etc/modprobe.d and underlying files are only writeable by the root user'
-  desc 'Various checks regarding modprobe.d modules for loadable modules for eg floppy, USB disk etc'
+  title 'Handling of removable media (physical and virtual)'
+  desc 'Verify that /etc/modprobe.d and underlying files have mode 0755 and are only writeable by owner'
   tag controlID: '0'
   tag checkID: '2'
   describe file('/etc/modprobe.d') do       
@@ -24,15 +30,13 @@ control 'TB_Linux_controlID_0.2' do		# A unique ID for this control
 end
 
 
-control 'TB_Linux_controlID_0.3' do		# A unique ID for this control
+control 'TB_Linux_controlID_0.3' do		
   impact 0.4                                	
-  title 'Verify that /etc/modprobe.d and underlying files are only writeable by the root user'
-  desc 'Various checks regarding modprobe.d modules for loadable modules for eg floppy, USB disk etc'
+  title 'Handling of removable media (physical and virtual)'
+  desc 'Verify that /etc/modprobe.d and underlying files are owned by the root user'
   tag controlID: '0'
   tag checkID: '3'
   describe file('/etc/modprobe.d') do
     its('owner') { should cmp 'root' }
   end
 end
-
-
--- TB_Linux/controls/TB_Linux_controlID_6.rb
+++ TB_Linux/controls/TB_Linux_controlID_6.rb
@@ -1,16 +1,17 @@
-title 'Handling of logging'
+# Columns in final CSV / Excel sheet:
+# controlID	-> "Control ID"
+# title		-> "Name"
+# checkID	-> "Check ID"
+# desc		-> "Description"
+# describe	-> "Result"
 
-control 'TB_Linux_controlID_6.1' do		# A unique ID for this control
+control 'TB_Linux_controlID_6.1' do		
   impact 0.4                                	
-  title 'Verify that /var/log and underlying files are only writeable by the root user'
+  title 'Event logging storage and protection'
   desc 'Various checks regarding /var/log'
   tag controlID: '6'
   tag checkID: '1'
-
   describe file('/var/log') do
     its('owner') { should cmp 'root' }
   end
-
 end
-
-

40240860b16594a4c1592a6b0461718ad92d096a
Emiel Kuiper <emielkuiper@gmail.com>
2018-11-06 12:20:58 +0100

    Added extra to_elastic option in makefile

---
+++ TB_Linux/make_elastic_localfile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# export TIMESTAMP=`date --iso-8601`
+export TIMESTAMP=`date '+%Y-%m-%d %H:%M:%S'`
+
+cd controls && ls *.rb | while read line
+  do
+    inspec exec $line --reporter json| jq '.profiles[] | [{ Controls: .controls[] } | { Timestamp: env.TIMESTAMP, Hostname: env.HOSTNAME, ControlID: .Controls.tags.controlID, Name: .Controls.title, CheckID: .Controls.tags.checkID, Description: .Controls.desc, Results: .Controls.results[].status }] | .[]'
+  done
